67e4e7585ef3359d76fa2e251d67a13030bb3e4f
Ferran <jjzzhlhl@hotmail.com>
2016-08-20 20:53:32 +0800

    finish chapter XXII: more about intents and tasks.

--- app/src/main/java/com/ferran/nerdlauncher/Presenter/NerdLauncherFragment.java
+++ app/src/main/java/com/ferran/nerdlauncher/Presenter/NerdLauncherFragment.java
@@ -1,21 +1,38 @@
 package com.ferran.nerdlauncher.Presenter;
 
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.Icon;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DrawableUtils;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.ferran.nerdlauncher.R;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class NerdLauncherFragment extends Fragment {
+    private static final String TAG = "NerdLauncherFragment";
+
     private RecyclerView mRecyclerView;
 
     public NerdLauncherFragment() {
@@ -23,9 +40,9 @@ public class NerdLauncherFragment extends Fragment {
     }
 
     public static NerdLauncherFragment newInstance() {
-        
+
         Bundle args = new Bundle();
-        
+
         NerdLauncherFragment fragment = new NerdLauncherFragment();
         fragment.setArguments(args);
         return fragment;
@@ -34,11 +51,90 @@ public class NerdLauncherFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View v=inflater.inflate(R.layout.fragment_nerd_launcher,container,false);
-        mRecyclerView=(RecyclerView)v.findViewById(R.id.fragment_nerd_launcher_recycler_view);
+        View v = inflater.inflate(R.layout.fragment_nerd_launcher, container, false);
+        mRecyclerView = (RecyclerView) v.findViewById(R.id.fragment_nerd_launcher_recycler_view);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        setupAdapter();
 
         return v;
+    }
+
+    private void setupAdapter() {
+        Intent startupIntent = new Intent(Intent.ACTION_MAIN);
+        startupIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+        PackageManager pm = getActivity().getPackageManager();
+        List<ResolveInfo> activities = pm.queryIntentActivities(startupIntent, 0);
+        Collections.sort(activities, new Comparator<ResolveInfo>() {
+            @Override
+            public int compare(ResolveInfo lhs, ResolveInfo rhs) {
+                PackageManager pm = getActivity().getPackageManager();
+                return String.CASE_INSENSITIVE_ORDER.compare(
+                        lhs.loadLabel(pm).toString(), rhs.loadLabel(pm).toString());
+            }
+        });
+
+        Log.i(TAG, "Found " + activities.size() + " activities.");
+        mRecyclerView.setAdapter(new ActivityAdapter(activities));
+    }
+
+    private class ActivityHolder extends RecyclerView.ViewHolder {
+        private ResolveInfo mResolveInfo;
+        private TextView mNameTextView;
+        private ImageView mIconImageView;
+
+        public ActivityHolder(View itemView) {
+            super(itemView);
+            mNameTextView = (TextView) itemView.findViewById(R.id.list_application_name);
+            mIconImageView = (ImageView) itemView.findViewById(R.id.list_application_Icon);
+        }
+
+        public void bindActivity(ResolveInfo resolveInfo) {
+            mResolveInfo = resolveInfo;
+            PackageManager pm = getActivity().getPackageManager();
+            String appName = mResolveInfo.loadLabel(pm).toString();
+            mNameTextView.setText(appName);
+            mNameTextView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ActivityInfo activityInfo = mResolveInfo.activityInfo;
+                    Intent i = new Intent(Intent.ACTION_MAIN)
+                            .setClassName(activityInfo.applicationInfo.packageName, activityInfo.name)
+                            .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(i);
+                }
+            });
+            Drawable appIcon= mResolveInfo.loadIcon(pm);
+            mIconImageView.setImageDrawable(appIcon);
+            mIconImageView.setMaxHeight(144);
+            mIconImageView.setMaxWidth(144);
+        }
+    }
+
+    private class ActivityAdapter extends RecyclerView.Adapter<ActivityHolder> {
+        private final List<ResolveInfo> mActivities;
+
+        public ActivityAdapter(List<ResolveInfo> activities) {
+            mActivities = activities;
+        }
+
+        @Override
+        public int getItemCount() {
+            return mActivities.size();
+        }
+
+        @Override
+        public void onBindViewHolder(ActivityHolder holder, int position) {
+            ResolveInfo resolveInfo = mActivities.get(position);
+            holder.bindActivity(resolveInfo);
+        }
+
+        @Override
+        public ActivityHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+            View view = layoutInflater.inflate(R.layout.list_application, parent, false);
+            return new ActivityHolder(view);
+
+        }
     }
 
 }
